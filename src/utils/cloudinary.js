e45d55b75c5d908e08eaef3192d52e1032f65b89
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-28 13:02:50 +0530

    Implemented register controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -18,7 +18,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     // file has been uploaded successfull
     //console.log("file is uploaded on cloudinary ", response.url);
     console.log("file is uploaded on cloudinary ", response);
-    fs.unlinkSync(localFilePath)
+    //fs.unlinkSync(localFilePath)
     return response;
 
   } catch (error) {
