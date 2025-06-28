f711e3537c56989a42e21cb0db7c8cb84698ad1d
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-28 15:21:42 +0530

    cloudinary resopnse analysis

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,8 +17,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     })
     // file has been uploaded successfull
     //console.log("file is uploaded on cloudinary ", response.url);
-    console.log("file is uploaded on cloudinary ", response);
-    //fs.unlinkSync(localFilePath)
+    //console.log("file is uploaded on cloudinary ", response);
+    fs.unlinkSync(localFilePath)
     return response;
 
   } catch (error) {
